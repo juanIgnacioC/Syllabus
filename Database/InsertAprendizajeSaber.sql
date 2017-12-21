b0ad1547725762b4979e4fc0bfc0967f4df352d0
Unknown <aelguedaicc@gmail.com>
2017-12-21 03:14:24 -0300

    vista competencia aprendizaje + insertsTabMedias

---
+++ Database/InsertAprendizajeSaber.sql
@@ -0,0 +1,6 @@
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (1, 1);
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (1, 2);
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (1, 3);
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (1, 4);
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (2, 5);
+INSERT INTO syllabus.Aprendizaje_Saber (codigoAprendizaje, idSaber) VALUES (2, 6);
--- SaberesySoluciones/SaberesySoluciones/Controllers/CompetenciaEnAprendizajeController.cs
+++ SaberesySoluciones/SaberesySoluciones/Controllers/CompetenciaEnAprendizajeController.cs
@@ -1,10 +1,10 @@
 ﻿using SaberesSyllabus.Models;
 using SaberesySoluciones.Models;
+using SaberesSyllabus.Repositories;
+using SaberesySoluciones.Repositories;
 using SaberesySoluciones.ViewModel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SaberesySoluciones.Controllers
@@ -14,16 +14,59 @@ namespace SaberesySoluciones.Controllers
         // GET: CompetenciaEnAprendizaje
         public ActionResult Index()
         {
-            CompetenciaEnAprendizajeController competenciaAprendizaje = new CompetenciaEnAprendizajeController();
+            List<Competencia> competencias = Competencias.LeerTodo();
+            if (competencias == null)
+            {
+                competencias = new List<Competencia>();
+            }
+            foreach (Competencia comp in competencias)
+            {
+                comp.Aprendizajes = Competencias.LeerSubAprendizajes(comp.Codigo);
 
-            return View(competenciaAprendizaje);
+                //Ordenar las SubSaberes según el codigo para evitar que se vean desordenadas :)
+                //apr.Saberes.Sort((x, y) => x.Codigo.CompareTo(y.Codigo));
+
+                if (comp.Aprendizajes == null)
+                {
+                    comp.Aprendizajes = new List<Aprendizaje>();
+                }
+            }
+
+            return View(competencias);
+        }
+        /*
+        [HttpPost]
+        public ActionResult CargarCompetenciaes(int codigoSaber)
+        {
+            var competencias = Competenciaes.LeerSubSaberes(codigoSaber);
+            if (competencias != null)
+            {
+                saberCompetencia.CompetenciaDeSaber = ;
+            }
+            aprendizajeSaber.CodigoAprendizaje = codigoAprendizaje;
+
+            return RedirectToAction("Index", "AprendizajeEnSaber");
         }
 
         [HttpPost]
-        public ActionResult CargarAprendizajes(String codigo)
+        public ActionResult AgregarSaber(String codigo)
         {
+            if (aprendizajeSaber.CodigoAprendizaje != null)
+            {
+                var agregar = Saberes.CrearSaberEnAprendizaje(aprendizajeSaber.CodigoAprendizaje, codigo);
+            }
 
-            return RedirectToAction("Index", "CompetenciaEnAprendizajeController ");
+            return RedirectToAction("Index", "AprendizajeEnSaber");
         }
+
+        [HttpPost]
+        public ActionResult EliminarSaber(String codigo)
+        {
+            if (aprendizajeSaber.CodigoAprendizaje != null)
+            {
+                var agregar = Saberes.EliminarSaberEnAprendizaje(aprendizajeSaber.CodigoAprendizaje, codigo);
+            }
+            return RedirectToAction("Index", "AprendizajeEnSaber");
+        }*/
     }
 }
