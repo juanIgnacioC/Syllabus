47f2e08f921640b4a359640840081d24ac2bf590
Unknown <aelguedaicc@gmail.com>
2017-12-21 01:45:24 -0300

    vista unidad saber

---
+++ SaberesySoluciones/SaberesySoluciones/Controllers/SaberEnUnidadController.cs
@@ -0,0 +1,72 @@
+﻿using SaberesSyllabus.Models;
+using SaberesySoluciones.Models;
+using SaberesSyllabus.Repositories;
+using SaberesySoluciones.Repositories;
+using SaberesySoluciones.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SaberesySoluciones.Controllers
+{
+    public class SaberEnUnidadController : Controller
+    {
+        // GET: AprendizajeEnSaber
+        public ActionResult Index()
+        {
+            List<Unidad> unidades = Unidades.LeerTodo();
+            if (unidades == null)
+            {
+                unidades = new List<Unidad>();
+            }
+            foreach (Unidad uni in unidades)
+            {
+                uni.Saberes = Unidades.LeerSubSaberes(uni.Id);
+
+                //Ordenar las SubSaberes según el codigo para evitar que se vean desordenadas :)
+                //apr.Saberes.Sort((x, y) => x.Codigo.CompareTo(y.Codigo));
+
+                if (uni.Saberes == null)
+                {
+                    uni.Saberes = new List<Saber>();
+                }
+            }
+
+            return View(unidades);
+        }
+        /*
+        [HttpPost]
+        public ActionResult CargarUnidades(int codigoSaber)
+        {
+            var unidades = Unidades.LeerSubSaberes(codigoSaber);
+            if (unidades != null)
+            {
+                saberUnidad.UnidadDeSaber = ;
+            }
+            aprendizajeSaber.CodigoAprendizaje = codigoAprendizaje;
+
+            return RedirectToAction("Index", "AprendizajeEnSaber");
+        }
+
+        [HttpPost]
+        public ActionResult AgregarSaber(String codigo)
+        {
+            if (aprendizajeSaber.CodigoAprendizaje != null)
+            {
+                var agregar = Saberes.CrearSaberEnAprendizaje(aprendizajeSaber.CodigoAprendizaje, codigo);
+            }
+
+            return RedirectToAction("Index", "AprendizajeEnSaber");
+        }
+
+        [HttpPost]
+        public ActionResult EliminarSaber(String codigo)
+        {
+            if (aprendizajeSaber.CodigoAprendizaje != null)
+            {
+                var agregar = Saberes.EliminarSaberEnAprendizaje(aprendizajeSaber.CodigoAprendizaje, codigo);
+            }
+            return RedirectToAction("Index", "AprendizajeEnSaber");
+        }*/
+    }
+}
---
+++ SaberesySoluciones/SaberesySoluciones/ViewModel/SaberEnUnidadesController.cs
@@ -0,0 +1,24 @@
+﻿using SaberesSyllabus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaberesySoluciones.ViewModel
+{
+    public class SaberEnUnidadesController : Controller
+    {
+        public List<Saber> Saberes { get; set; }
+        public List<Unidad> Unidades { get; set; }
+        public List<Unidad> UnidadDeSaber { get; set; }
+        public string CodigoSaber { get; set; }
+
+        public SaberEnUnidadesController()
+        {
+            this.Saberes = new List<Saber>();
+            this.Unidades = new List<Unidad>();
+            this.UnidadDeSaber = new List<Unidad>();
+        }
+    }
+}
